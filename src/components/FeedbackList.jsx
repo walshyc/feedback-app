a12097db51394900b24614dd8d8c3c3d319367ae
CONOR GERARD WALSH <conor.a.walsh.2021@mumail.ie>
2021-12-10 16:59:31 +0000

    added animation with Framer-Motion

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,18 +1,35 @@
+import { motion, AnimatePresence } from 'framer-motion';
 import FeedbackItem from './FeedbackItem';
 
 const FeedbackList = ({ feedback, handleDelete }) => {
   if (!feedback || feedback.length === 0) {
     return <p>No feedback yet</p>;
   }
+  //   return (
+  //     <div className='grid grid-cols-1 sm:grid-cols-2 gap-8'>
+  //       {feedback.map((fb) => (
+  //         <FeedbackItem
+  //           key={fb.id}
+  //           item={fb}
+  //           handleDelete={handleDelete}
+  //         ></FeedbackItem>
+  //       ))}
+  //     </div>
+  //   );
   return (
-    <div className='grid grid-cols-1 sm:grid-cols-2 gap-8'>
-      {feedback.map((fb) => (
-        <FeedbackItem
-          key={fb.id}
-          item={fb}
-          handleDelete={handleDelete}
-        ></FeedbackItem>
-      ))}
+    <div className="grid grid-cols-1 sm:grid-cols-2 gap-8">
+      <AnimatePresence>
+        {feedback.map((fb) => (
+          <motion.div
+            key={fb.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem item={fb} handleDelete={handleDelete}></FeedbackItem>
+          </motion.div>
+        ))}
+      </AnimatePresence>
     </div>
   );
 };
